8aa7b149b5ac75014b9c875899aa39c7450b211c
Lucas Santos <114298847+Lucaszcx@users.noreply.github.com>
2023-02-07 19:48:05 -0300

    Adicionando todos os arquivos do projeto

---
+++ js/calcula-imc.js
@@ -0,0 +1,61 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida nutricionista"
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i = 0; i < pacientes.length; i++) {
+
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoValido = validaPeso(peso);
+    var alturaValida = validaAltura(altura);
+
+    if(!pesoValido) {
+        pesoValido = false;
+        tdImc.textContent = "Peso inválido!";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(!alturaValida) {
+        alturaValida = false;
+        tdImc.textContent = "Altura inválida!"
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(pesoValido && alturaValida) {
+    var imc = calculaimc(peso, altura);
+    tdImc.textContent = imc;
+    }
+}
+
+function validaPeso(peso) {
+    if (peso >= 0 && peso < 1000) {
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function validaAltura(altura) {
+    if (altura >= 0 && altura <= 3.00) {
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function calculaimc(peso, altura) {
+    var imc = 0;
+
+    imc = peso / (altura * altura);
+
+    return imc.toFixed(2);;
+}
